d2302f10c2d2cefe650461aca89f5584137fb20e
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-18 12:59:49 -0400

    non blocking code

--- app.js
+++ app.js
@@ -21,16 +21,18 @@ const server = http.createServer((req, res) => {
     });
     // fires at the end
     // this a callback and will run a synchronosly. It will register and run after the
-    req.on("end", () => {
+    return req.on("end", () => {
       //create a new buffer and add all chunks to it
       const parsedBody = Buffer.concat(body).toString();
       console.log(parsedBody);
       //creates 2 objects in an array and gets the second one
       const message = parsedBody.split("="[1]);
-      fs.writeFileSync("message.txt", message);
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
+      fs.writeFile("message.txt", message, err => {
+        // actions that we want after we finish writing the file
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
   }
   res.setHeader("Content-Type", "text/html");
