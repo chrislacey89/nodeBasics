37cd08b0e97a47a6b8f4c628b4cb46288afe8581
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-30 14:21:54 -0400

    added static pages

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const shopRoutes = require("./routes/shop");
 
 //registers a middleware. It parses bodies sent through a form. then it calls next to go on to the next function
 app.use(bodyParser.urlencoded({ extended: false }));
+// allows use of static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // admin is a filter. Only paths with /admin goes to adminRouts file
 app.use("/admin", adminRoutes);
