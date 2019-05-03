eeb5cc31437542ec6ae40ddf03592da82be559e7
Chris Lacey <ChrisLacey89@gmail.com>
2019-05-03 07:07:54 -0400

    added pug

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// set any values globally on our app configuration
+// tell express that we want to complile dynamic templates with pug engine
+app.set("view engine", "pug");
+//tell express wehre we want to compile them
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -14,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // admin is a filter. Only paths with /admin goes to adminRouts file
+
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,7 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  //dirname holds aboslute path on our OS to this project folder
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
