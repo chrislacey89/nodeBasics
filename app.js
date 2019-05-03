98319cff2d193fb23ba32b861546677b5b4eb855
Chris Lacey <ChrisLacey89@gmail.com>
2019-05-02 20:18:20 -0400

    Sharing Data Across Requests & Users

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 //registers a middleware. It parses bodies sent through a form. then it calls next to go on to the next function
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // admin is a filter. Only paths with /admin goes to adminRouts file
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(adminData.products);
   //dirname holds aboslute path on our OS to this project folder
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
