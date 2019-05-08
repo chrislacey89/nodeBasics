c48d0236160eaa812d7ceb14fe1e6674fe390c25
Chris Lacey <ChrisLacey89@gmail.com>
2019-05-08 13:22:46 -0400

    store data in files with the model

--- models/product.js
+++ models/product.js
@@ -1,15 +1,59 @@
+// import core file system module
+const fs = require("fs");
+// construct a path that works on all OS
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    // access data folder in root project folder
+    // store file called products.json
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    //reads entire content of file
+    // will either get error or the data
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      // if not an error then reach products from the file we read. JSON is a helper object. Parse takes JSON and gives us back an object or array that we can work with
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      // push new product(this) on to products array
+      products.push(this);
+      // strinify takes JS array and converts into JSON
+      fs.writeFile(
+        p,
+        JSON.stringify(products),
+        //callback in case we get an error
+        err => {
+          console.log(err);
+        }
+      );
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+
+      // it is returned as text so we need JSON.parse
+      return JSON.parse(fileContent);
+    });
+    return products;
+  }
+};
